a49095052cf7d429979e748cdc4466b1ccf9b037
lea-apotheloz <apothelozlea2@gmail.com>
2025-03-03 14:25:21 +0100

    feat: user is able to add a new todo

--- src/TodoCreationForm.tsx
+++ src/TodoCreationForm.tsx
@@ -1,20 +1,44 @@
-const TodoCreationForm = () => {
-  return (
-    <form>
-      <label className="TodoForm">
-        <input type={'date'} placeholder={'due Date'} />
-        <input type="text" placeholder={'Enter a new Todo'} />
-        <button> Add Todo</button>
-      </label>
+import React, {useState} from "react";
+import TodoList from "./TodoList.tsx";
 
-      <select>
-        <option>SORT</option>
-        <option>Due date</option>
-        <option>Name</option>
-        <option>Done</option>
-        <option>Undone</option>
-      </select>
-    </form>
-  );
-};
+const TodoCreationForm = ({list, addtodolist}:{list:string[] ;addtodolist: (addtodolist: string) => void}) => {
+
+    const [value, setValue] = useState('');
+
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value)
+    }
+
+    const addlist = (event: any) => {
+        event.preventDefault();
+        if (!value) return
+
+
+       addtodolist(value)
+        setValue('')
+    }
+
+    return (
+        <form onSubmit={addlist}>
+            <label className="TodoForm">
+                <input type={'date'} placeholder={'due Date'}/>
+                <input type="text" placeholder={'Enter a new Todo'} value={value} onChange={handleInput}/>
+                <button> Add Todo</button>
+
+            </label>
+
+            <select>
+                <option>SORT</option>
+                <option>Due date</option>
+                <option>Name</option>
+                <option>Done</option>
+                <option>Undone</option>
+            </select>
+
+            <TodoList list={list}/>
+
+        </form>
+    )
+}
 export default TodoCreationForm;
